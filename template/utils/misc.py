e538e8c33e15855f0772c0f6c7f60bb8cb357032
mekaneeky <alizawahry1@gmail.com>
2023-12-10 20:59:12 +0300

    fix wandb signature bug

--- template/utils/misc.py
+++ template/utils/misc.py
@@ -145,7 +145,7 @@ class AsyncDendritePool:
 def load_wandb(config, wallet):
 
     #signature = wallet.hotkey.sign(config.neuron.run_id).hex() #Extra for verification if needed
-    run_name = config.neuron.run_id #+ signature 
+    run_name = config.neuron.run_id + "_" + wallet.hotkey.ss58_address#+ signature 
     wandb_run = wandb.init(
         id = run_name,
         name=run_name,
@@ -157,7 +157,6 @@ def load_wandb(config, wallet):
     )
 
     signature = wallet.hotkey.sign(config.neuron.run_id.encode()).hex()
-    config.hotkey_address = wallet.hotkey.ss58_address
     config.signature = signature
     wandb_run.config.update(config, allow_val_change=True)
     return wandb_run
