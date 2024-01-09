4b21e70bf1697d08ce52d1c070d2c5bc46b3767a
mikkelmedm <thybo11@hotmail.com>
2024-01-09 11:34:48 +0000

    rm line

--- template/validator/validator_core.py
+++ template/validator/validator_core.py
@@ -83,7 +83,6 @@ class DatasetState:
 
     def get_dataset_indices_test(self, batch_size):
         dataset_indices_train = self.get_dht("dataset_indices_train")
-        print(dataset_indices_train)
         start = random.choice(range(len(dataset_indices_train) - batch_size + 1))
         dataset_indices_test = dataset_indices_train[start:start + batch_size]
         self.set_dht("dataset_indices_test", dataset_indices_test)
