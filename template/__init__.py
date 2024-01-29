0f9401cffbaefe2e0e5036c1575b980721ecd7ef
Karim Foda <35491698+KMFODA@users.noreply.github.com>
2024-01-29 22:25:37 +0200

    Update __init__.py

--- template/__init__.py
+++ template/__init__.py
@@ -18,6 +18,7 @@
 # DEALINGS IN THE SOFTWARE.
 
 # version nomenclature = __training_type__.__model__.__other_changes__
+__version__ = "0.0.3"
 version_split = __version__.split(".")
 __spec_version__ = (1000 * int(version_split[0])) + (10 * int(version_split[1])) + (1 * int(version_split[2]))
 
