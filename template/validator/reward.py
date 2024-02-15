6f44787c2d66b3ddcfb09ace28bf4c768c04b03d
mekaneeky <alizawahry1@gmail.com>
2024-02-16 01:56:57 +0300

    anomaly loss

--- template/validator/reward.py
+++ template/validator/reward.py
@@ -197,6 +197,7 @@ def get_rewards(
 
     # Assign a score of 0 to non-responders.
     scores[~responded] = 0.1
+    scores = torch.FloatTensor([score for score in scores])
 
     bt.logging.info(f"Global Scores:        {scores}")
 
