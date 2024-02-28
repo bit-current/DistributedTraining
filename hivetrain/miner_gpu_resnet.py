b060f30cb71805c5d5a1b5d8c526419548792009
mekaneeky <alizawahry1@gmail.com>
2024-02-28 03:56:27 +0300

    ...

--- hivetrain/miner_gpu_resnet.py
+++ hivetrain/miner_gpu_resnet.py
@@ -87,6 +87,7 @@ def main():
     # Initializes the distributed backend which will take care of sychronizing nodes/GPUs
     torch.distributed.init_process_group(backend="nccl", 
         init_method=f"tcp://{argv.master_addr}:{argv.master_port}",
+        rank=argv.rank,
         world_size=2
     )
     # torch.distributed.init_process_group(backend="gloo")
