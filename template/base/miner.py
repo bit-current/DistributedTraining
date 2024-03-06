9980a6d5a5009ee906ad956bf6798097bb02a5af
mekaneeky <alizawahry1@gmail.com>
2024-03-06 07:16:53 +0300

    dht loading bug fix

--- template/base/miner.py
+++ template/base/miner.py
@@ -24,6 +24,22 @@ import traceback
 import bittensor as bt
 from template.base.neuron import BaseNeuron
 
+import re
+import hivemind
+import requests
+import torch
+import wandb
+from datasets import load_dataset
+from hivemind import utils
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    default_data_collator,
+    get_linear_schedule_with_warmup,
+)
+
 
 class BaseMinerNeuron(BaseNeuron):
     """
@@ -43,8 +59,81 @@ class BaseMinerNeuron(BaseNeuron):
                 "You are allowing non-registered entities to send requests to your miner. This is a security risk."
             )
 
+       
+        version = "4"
+        address = self.config.dht.announce_ip
+        announce_maddrs = [f"/ip{version}/{address}/tcp/{self.config.dht.port}"]
+
+        # Init list of available DHT addresses from wandb
+        try:
+            self.dht = hivemind.DHT(
+                        host_maddrs=[
+                            f"/ip4/0.0.0.0/tcp/{self.config.dht.port}",
+                            f"/ip4/0.0.0.0/udp/{self.config.dht.port}/quic",
+                        ],
+                        initial_peers=self.config.neuron.initial_peers,
+                        announce_maddrs=announce_maddrs,
+                        start=True,
+                    )
+            bt.logging.info(
+                        f"Successfully initialised ORIGINAL dht using initial_peer as {self.config.neuron.initial_peers}"
+                    )
+        except:
+            api = wandb.Api()
+            initial_peers_list = set(self.config.neuron.initial_peers)
+            runs = api.runs(
+                f"{self.config.neuron.wandb_entity}/{self.config.neuron.wandb_project}"
+            )
+            bt.logging.info(
+                        f"Successfully initialised dht using initial_peer as {}"
+                    )
+            for ru in runs:
+                if ru.state == "running":
+                    initial_peers_list.update(ru.config["neuron"]["initial_peers"])
+            initial_peers_list = list(initial_peers_list)
+            # Init DHT
+            retries = 0
+            while retries <= len(initial_peers_list):
+                if retries == len(initial_peers_list):
+                    raise Exception("Max retries reached, operation failed.")
+                try:
+                    # Init DHT
+                    self.dht = hivemind.DHT(
+                        host_maddrs=[
+                            f"/ip4/0.0.0.0/tcp/{self.config.dht.port}",
+                            f"/ip4/0.0.0.0/udp/{self.config.dht.port}/quic",
+                        ],
+                        initial_peers=[initial_peers_list[retries]],
+                        announce_maddrs=announce_maddrs,
+                        start=True,
+                    )
+                    bt.logging.info(
+                        f"Successfully initialised dht using initial_peer as {initial_peers_list[retries]}"
+                    )
+                    break
+                except Exception as e:
+                    bt.logging.error(
+                        f"Attempt {retries + 1} to init DHT using initial_peer as {initial_peers_list[retries]} failed with error: {e}"
+                    )
+                    retries += 1
+                    bt.logging.error(f"Retrying...")
+        utils.log_visible_maddrs(self.dht.get_visible_maddrs(), only_p2p=True)
+
+        # Add DHT address to wandb config
+        self.config.neuron.initial_peers = self.config.neuron.initial_peers + [
+            re.sub("ip4/?(.*?)/", f"ip{version}/{address}/", str(addr), flags=re.DOTALL)
+            for addr in self.dht.get_visible_maddrs()
+        ]
+
         # The axon handles request processing, allowing validators to send this miner requests.
-        self.axon = bt.axon(wallet=self.wallet, port=self.config.axon.port)
+        # https://github.com/opentensor/bittensor/blob/935217242ef8b48ecf0d63f9bb0b920905c8cda8/bittensor/axon.py#L331
+        self.axon = bt.axon(
+            wallet=self.wallet,
+            port=self.config.axon.port,
+            external_port=self.config.axon.external_port,
+            ip=self.config.dht.announce_ip,
+            external_ip=self.config.dht.announce_ip,
+        )
 
         # Attach determiners which functions are called when servicing a request.
         bt.logging.info(f"Attaching forward function to miner axon.")
