006678c099766ada0c2ddef54a8528fdb8d4c1c8
mekaneeky <alizawahry1@gmail.com>
2024-03-22 13:51:11 +0300

    Update run_validator.sh

--- run_validator.sh
+++ run_validator.sh
@@ -9,7 +9,7 @@ args=()
 version_location="./template/__init__.py"
 version="__version__"
 repo="bit-current/DistributedTraining"
-branch="dev_kb"
+branch="main"
 repo_url="https://github.com/$repo.git"
 
 old_args=$@
