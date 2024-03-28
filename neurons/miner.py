7f48d1e3023968af5e3134aa8ad708f40c52efbe
Kofi Osei-Bonsu <44671331+kobetryz@users.noreply.github.com>
2024-03-27 21:52:07 -0400

    testing auto update

--- neurons/miner.py
+++ neurons/miner.py
@@ -165,6 +165,7 @@ class StreamingDataset(IterableDataset):
 
 # prepare a dataset for use with training
 dataset = StreamingDataModule(tokenizer, dataset_config)
+print('This is working')
 
 
 # wrap the LightningModule in a custom class
