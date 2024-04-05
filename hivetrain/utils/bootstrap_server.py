4f38b8942ee4d8c3fdc07e81b67aa843bbeb31ac
mekaneeky <alizawahry1@gmail.com>
2024-04-06 00:08:43 +0300

    no caching

--- hivetrain/utils/bootstrap_server.py
+++ hivetrain/utils/bootstrap_server.py
@@ -115,13 +115,14 @@ def check_and_manage_dhts():
         for _ in range(5 - len(dht_list)):
             tcp_port, udp_port = get_random_ports(allowed_ports, num_ports=2)
             new_dht = hivemind.DHT(
-                host_maddrs=[f"/ip4/{args.host_address}/tcp/{tcp_port}"],
+                host_maddrs=[f"/ip4/{args.host_address}/tcp/{tcp_port}", f"/ip4/{args.host_address}/udp/{udp_port}/quic"],
                 use_relay=False,
                 use_auto_relay=False,
-                announce_maddrs=[f"/ip4/{args.external_address}/tcp/{tcp_port}"],
+                announce_maddrs=[f"/ip4/{args.external_address}/tcp/{tcp_port}", f"/ip4/{args.external_address}/udp/{udp_port}"],
+                #identity_path=args.local_DHT_file
+                initial_peers=initial_peers,
                 num_replicas=0,
                 cache_nearest=False,
-                initial_peers=initial_peers,
                 start=True
             )
             new_dht.wait_until_ready()
@@ -160,12 +161,12 @@ def return_dht_address():
             tcp_port, udp_port = get_random_ports(allowed_ports, num_ports=2)
             logger.info("Get DHT")
             new_dht = hivemind.DHT(
-                host_maddrs=[f"/ip4/{args.host_address}/tcp/{tcp_port}"],
+                host_maddrs=[f"/ip4/{args.host_address}/tcp/{tcp_port}", f"/ip4/{args.host_address}/udp/{udp_port}/quic"],
                 use_relay=False,
                 use_auto_relay=False,
                 num_replicas=0,
                 cache_nearest=False,
-                announce_maddrs=[f"/ip4/{args.external_address}/tcp/{tcp_port}"],
+                announce_maddrs=[f"/ip4/{args.external_address}/tcp/{tcp_port}", f"/ip4/{args.external_address}/udp/{udp_port}"],
                 #announce_maddrs=[f"/ip4/{args.host_address}"],
                 start=True
             )
