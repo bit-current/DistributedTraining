latest: averager fixes

hivetrain/averaging_logic.py
@@ -32,6 +32,9 @@ class Averager:
 
             # Load the gradients directly using torch.load
             aggregated_gradients = torch.load(gradient_file_path)
+
+            if self.have_nans(aggregated_gradients):
+                return None
                 
             return aggregated_gradients
         except Exception as e:
@@ -43,10 +46,21 @@ class Averager:
         self.bittensor_network.sync(lite=False)#scope issue FIXME?
             
         validator_uids = self.bittensor_network.get_validator_uids(vpermit_tao_limit=1024)
-        #breakpoint()
-        #validator_combined_weights = torch.mean(self.bittensor_network.metagraph.W[validator_uids, :],axis=0)
-        n = len(self.bittensor_network.metagraph.hotkeys) #FIXME I am only for testing NOPROD
-        self.validator_combined_weights = torch.full((n,1), 1/n, dtype=torch.float32) #FIXME I am only for testing NOPROD
+
+        if isinstance(self.bittensor_network.metagraph.W, list):
+            self.validator_combined_weights = []
+            weights_list = []
+            for validator_uid in validator_uids:
+                weights = self.bittensor_network.metagraph.W[validator_uid]
+                if sum(weights)==0:
+                    continue
+                else:
+                    weights_list.append(weights)
+            self.validator_combined_weights = torch.mean(torch.tensor(weights_list), axis=0)
+        else:
+            self.validator_combined_weights = torch.mean(self.bittensor_network.metagraph.W[validator_uids, :], axis=0)
+        #n = len(self.bittensor_network.metagraph.hotkeys) #FIXME I am only for testing NOPROD
+        #self.validator_combined_weights = torch.full((n,1), 1/n, dtype=torch.float32) #FIXME I am only for testing NOPROD
         # Get average of validator weights weighted by their stake?
         self.miner_gradients = []
         self.miner_weights = []
@@ -61,7 +75,14 @@ class Averager:
                 self.miner_gradients.append(None)
                 self.miner_weights.append(0.0)
                 self.miner_hotkeys.append(hotkey)
-        
+
+    @staticmethod
+    def have_nans(aggregated_gradients):
+        for tensor in aggregated_gradients.values():
+            if torch.isnan(tensor).any():
+                logging.debug("NaN values detected in the aggregated gradients.")
+                return True
+        return False
 
     def average_gradients(self, beta=1.0):
 
@@ -119,8 +140,53 @@ class Averager:
             time.sleep(time_to_wait)
             logging.info("Averaging Done")
 
+class DeltaAverager(Averager):
+    def __init__(self, model, local_dir, repo_id,chain_manager,bittensor_network, hf_token=os.environ.get("HF_TOKEN")):
+        self.model = model
+        self.local_dir = local_dir
+        self.repo_id = repo_id
+        self.hf_token = hf_token
+        self.scored_gradients = None
+        self.last_sync_time = 0
+        self.bittensor_network = bittensor_network
+        self.chain_manager = chain_manager
 
-class LocalAverager(Averager):
+    def average_gradients(self, beta=1.0):
+
+        self.non_none_miner_gradients = [gradients for gradients in self.miner_gradients if gradients is not None]
+        assert len(self.non_none_miner_gradients) > 0
+        averaged_gradients = {name: torch.zeros_like(grad) for name, grad in self.non_none_miner_gradients[0].items()}
+
+        for score, gradients in zip(self.validator_combined_weights, self.miner_gradients):
+            logging.info("Averaging Gradient")
+            if gradients is not None:
+                for (name, grad), (param_name, param) in zip(gradients.items(), self.model.named_parameters()):
+                    averaged_gradients[name] += (param + grad) * score * beta
+        
+        return averaged_gradients
+
+    def apply_averaged_gradients(self, averaged_gradients, alpha=0.00001):
+        
+        with torch.no_grad():
+            for name, param in self.model.named_parameters():
+                if name in averaged_gradients:
+                    param = averaged_gradients[name]
+
+    def run_periodic_averaging(self, t):
+        while True:
+            logging.info("Averaging Beggining")
+            start_time = time.time()
+            self.receive_and_score_gradients()
+            averaged_weights = self.average_gradients()
+            self.apply_averaged_gradients(averaged_weights)
+            self.save_model()
+            self.push_to_hf_hub(commit_message="Updated model with new gradients")
+            elapsed_time = time.time() - start_time
+            time_to_wait = max(0, t - elapsed_time)
+            time.sleep(time_to_wait)
+            logging.info("Averaging Done")
+
+class LocalAverager(DeltaAverager):
     def __init__(self, model, local_dir, chain_manager, bittensor_network=None, hf_token=None):
         super().__init__(model, local_dir, chain_manager=chain_manager,repo_id=None, bittensor_network=bittensor_network, hf_token=hf_token)
         # No need for repo_id or hf_token in the local version
@@ -139,6 +205,10 @@ class LocalAverager(Averager):
 
             # Load the gradients directly using torch.load
             aggregated_gradients = torch.load(gradient_file_path)
+            
+            if self.have_nans(aggregated_gradients):
+                return None
+
             return aggregated_gradients
         except Exception as e:
             logging.error(f"Error receiving gradients locally: {e}")

hivetrain/training_manager.py
@@ -42,7 +42,7 @@ class TrainingLoop:
         repo.git_push()
 
     def train(self, epochs, hf_manager):
-        last_send_time = time.time()
+        self.last_send_time = time.time()
         self.optimizer.zero_grad()
         self.aggregated_gradients = {name: torch.zeros_like(param) for name, param in self.model.named_parameters() if param.requires_grad}
         for epoch in range(epochs):
@@ -75,7 +75,7 @@ class TrainingLoop:
 
                 # Example of a condition to periodically send gradients
                 current_time = time.time() ##time.time()%sth works better
-                if current_time - last_send_time >= self.send_interval:
+                if current_time - self.last_send_time >= self.send_interval:
                     
                     average_loss = total_loss / total_examples
                     perplexity = math.exp(average_loss)
@@ -88,7 +88,7 @@ class TrainingLoop:
                     except Exception as e:
                         logging.warning(f"Sending gradients failed: {e}")
                         continue
-                    last_send_time = current_time
+                    self.last_send_time = current_time
                     # Reset aggregated gradients
                     #self.aggregated_gradients = {name: torch.zeros_like(param) for name, param in self.model.named_parameters() if param.requires_grad}
 
@@ -175,7 +175,7 @@ class MNISTTrain(LocalTrainingLoop):
 
     
     def train(self, epochs, hf_manager, n_steps):
-        last_send_time = time.time()
+        self.last_send_time = time.time()
         step_counter = 0  # Initialize step counter that persists across epochs
         test_counter = 0
         test_losses = []
@@ -257,7 +257,7 @@ class MNISTTrain(LocalTrainingLoop):
                 step_counter += 1  # Increment step counter after processing each batch
 
                 # Periodic actions such as logging and sending gradients
-                if time.time() - last_send_time >= self.send_interval:
+                if time.time() - self.last_send_time >= self.send_interval:
                     average_loss = total_loss / total_examples
                     logging.info(f"Epoch: {epoch}, Batch: {batch_idx}, Loss: {average_loss:.4f}")
 
@@ -265,7 +265,7 @@ class MNISTTrain(LocalTrainingLoop):
                     self.aggregated_gradients = {name: param.grad.clone() for name, param in self.model.named_parameters() if param.requires_grad and param.grad is not None}
                     self.store_gradients(self.aggregated_gradients, self.gradients_dir)
 
-                    last_send_time = time.time()
+                    self.last_send_time = time.time()
                     #total_loss = 0
                     #total_examples = 0  # Reset for the next interval
 
@@ -307,6 +307,8 @@ class MNISTDeltaTrain(LocalTrainingLoop):
         self.gradients_dir = gradients_dir
         self.averaging_dir = averaging_dir
 
+        self.last_send_time = time.time()
+
     def save_model(self):
         """
         Saves the model to the specified local directory.
@@ -344,7 +346,6 @@ class MNISTDeltaTrain(LocalTrainingLoop):
 
     
     def train(self, epochs, hf_manager, n_steps):
-        last_send_time = time.time()
         step_counter = 0  # Initialize step counter that persists across epochs
         test_counter = 0
         test_losses = []
@@ -363,14 +364,17 @@ class MNISTDeltaTrain(LocalTrainingLoop):
             total_loss = 0
             total_examples = 0
 
-            if hf_manager.check_for_new_submissions():
-                logging.info("Model updated from Hugging Face. Continuing training with new model...")
-                self.model = hf_manager.update_model(self.model)
-                self.optimizer = SGD(self.model.parameters(), lr=0.1)  # Reinitialize the optimizer
-                self.base_weights = {name: param.clone() for name, param in self.model.named_parameters()}
-                #self.optimizer.zero_grad()  # Ensure gradients are reset after model update
+            
 
             for batch_idx, (data, target) in enumerate(self.data_loader):
+
+                if hf_manager.check_for_new_submissions():#FIXME add this in other training manager classes
+                    logging.info("Model updated from Hugging Face. Continuing training with new model...")
+                    self.model = hf_manager.update_model(self.model)
+                    self.optimizer = SGD(self.model.parameters(), lr=0.001)  # Reinitialize the optimizer
+                    self.base_weights = {name: param.clone() for name, param in self.model.named_parameters()}
+                    #self.optimizer.zero_grad()  # Ensure gradients are reset after model update
+                    
                 output = self.model(data)
                 loss = F.cross_entropy(output, target)
                 loss.backward()
@@ -405,7 +409,7 @@ class MNISTDeltaTrain(LocalTrainingLoop):
                 step_counter += 1  # Increment step counter after processing each batch
 
                 # Periodic actions such as logging and sending gradients
-                if time.time() - last_send_time >= self.send_interval:
+                if time.time() - self.last_send_time >= self.send_interval:
                     average_loss = total_loss / total_examples
                     logging.info(f"Epoch: {epoch}, Batch: {batch_idx}, Loss: {average_loss:.4f}")
 
@@ -413,10 +417,9 @@ class MNISTDeltaTrain(LocalTrainingLoop):
                     self.weight_diffs = {name:  param.data - self.base_weights[name] for name, param in self.model.named_parameters() if param.requires_grad}
                     self.store_gradients(self.weight_diffs, self.gradients_dir)
 
-
                     logging.info(f"Model hash is: {self.calculate_model_hash()}")
 
-                    last_send_time = time.time()
+                    self.last_send_time = time.time()
                     #total_loss = 0
                     #total_examples = 0  # Reset for the next interval
 

neurons/validator_delta.py
@@ -8,7 +8,7 @@ import bittensor as bt
 from torch.utils.data import DataLoader, Dataset, IterableDataset
 
 from hivetrain.btt_connector import (
-    BittensorNetwork,
+    LocalBittensorNetwork,
     # get_validator_uids_and_addresses,
     serve_axon,
 )
@@ -30,17 +30,17 @@ from hivetrain.training_manager import FeedforwardNN
 
 args = Configurator.combine_configs()
 
-BittensorNetwork.initialize(args)
-my_hotkey = BittensorNetwork.wallet.hotkey.ss58_address
-my_uid = BittensorNetwork.metagraph.hotkeys.index(my_hotkey)
+LocalBittensorNetwork.initialize(args)
+my_hotkey = LocalBittensorNetwork.wallet.hotkey.ss58_address
+my_uid = LocalBittensorNetwork.metagraph.hotkeys.index(my_hotkey)
 
-address_store = LocalAddressStore(BittensorNetwork.subtensor, args.netuid,BittensorNetwork.wallet)
+address_store = LocalAddressStore(LocalBittensorNetwork.subtensor, args.netuid,LocalBittensorNetwork.wallet)
 
 
 batch_size = args.batch_size
 epochs = 10  # Adjust epochs for MNIST training, 30_000_000_000_000_000 is unrealistic
 learning_rate = 5e-5
-receive_interval = 1  # Every 60 seconds
+receive_interval = 30  # Every 60 seconds
 
 # Load the MNIST dataset
 transform = transforms.Compose([
@@ -59,6 +59,6 @@ optimizer = SGD(model.parameters(),lr = 0.1)
 #    def __init__(self, model, optimizer, data_loader, bittensor_network=None, chain_manager=None, interval=3600, local_gradient_dir="local_gradients"):
 hf_manager = LocalHFManager(repo_id=args.storage.model_dir)
 validator = MNISTDeltaValidator(model=model,optimizer=optimizer, data_loader=test_loader,
-    bittensor_network=BittensorNetwork ,hf_manager=hf_manager, interval=receive_interval, chain_manager=address_store,local_gradient_dir=args.storage.gradient_dir,
+    bittensor_network=LocalBittensorNetwork ,hf_manager=hf_manager, interval=receive_interval, chain_manager=address_store,local_gradient_dir=args.storage.gradient_dir,
     )
 validator.start_periodic_validation()