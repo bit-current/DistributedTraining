636192e4b96a20b97ac905508882a6da77fbb976
mekaneeky <alizawahry1@gmail.com>
2024-04-22 10:16:30 +0300

    Averager latest

--- hivetrain/averaging_logic.py
+++ hivetrain/averaging_logic.py
@@ -290,7 +290,7 @@ class ParameterizedAverager(DeltaAverager):
                     continue
                 base_model = torch.load(os.path.join(self.hf_manager.get_local_model_directory(),"averaged_model.pt"), map_location='cpu')#self.model.state_dict()
                 for name, delta_param in weight_delta.items():
-                    weight_delta[name] = weight_delta[name] + base_model[name]
+                    weight_delta[name].to(self.device) = weight_delta[name].to(self.device) + base_model[name].to(self.device)
                 yield weight_delta
 
     def get_averaged_model(self):
