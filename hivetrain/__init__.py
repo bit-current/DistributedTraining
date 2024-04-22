c9f1dc6e1764ac87998ee378aa8777ee9c4ded43
mekaneeky <alizawahry1@gmail.com>
2024-04-22 05:15:21 +0300

    Averager latest

--- hivetrain/__init__.py
+++ hivetrain/__init__.py
@@ -3,7 +3,7 @@ version_split = __version__.split(".")
 __spec_version__ = (100 * int(version_split[0])) + (10 * int(version_split[1])) + (1 * int(version_split[2]))
 
 from . import btt_connector
-from . import dht_connector
+#from . import dht_connector
 from . import auth
 from . import chain_manager
 from . import validation_logic
