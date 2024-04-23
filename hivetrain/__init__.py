feadfafb4f1a7ad42a8fcfcd28008e4159e5b5be
kofi Osei-Bonsu <kofiobonsu@gmail.com>
2024-04-23 12:13:28 -0400

    minor edits

--- hivetrain/__init__.py
+++ hivetrain/__init__.py
@@ -1,10 +1,10 @@
-# __version__ = "0.3.0"
-# version_split = __version__.split(".")
-# __spec_version__ = (100 * int(version_split[0])) + (10 * int(version_split[1])) + (1 * int(version_split[2]))
+__version__ = "0.3.0"
+version_split = __version__.split(".")
+__spec_version__ = (100 * int(version_split[0])) + (10 * int(version_split[1])) + (1 * int(version_split[2]))
 
-# from . import btt_connector
-# from . import chain_manager
-# from . import validation_logic
-# from . import averaging_logic
-# from . import hf_manager
-# from . import training_manager
+from . import btt_connector
+from . import chain_manager
+from . import validation_logic
+from . import averaging_logic
+from . import hf_manager
+from . import training_manager
