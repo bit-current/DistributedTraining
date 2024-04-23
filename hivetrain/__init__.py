c054c4f49bcc63579d65d26f270bdb1e898148d8
kofi Osei-Bonsu <kofiobonsu@gmail.com>
2024-04-23 12:11:48 -0400

    minor edits

--- hivetrain/__init__.py
+++ hivetrain/__init__.py
@@ -1,6 +1,6 @@
-__version__ = "0.3.0"
-version_split = __version__.split(".")
-__spec_version__ = (100 * int(version_split[0])) + (10 * int(version_split[1])) + (1 * int(version_split[2]))
+# __version__ = "0.3.0"
+# version_split = __version__.split(".")
+# __spec_version__ = (100 * int(version_split[0])) + (10 * int(version_split[1])) + (1 * int(version_split[2]))
 
 # from . import btt_connector
 # from . import chain_manager
